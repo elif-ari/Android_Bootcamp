cc59a379a68c8c3cb1e98555496f64d81ba4e061
Elif <43elif1305@gmail.com>
2025-04-26 23:56:03 +0300

    ilk ödev yuklendi

---
+++ Odev_1.kt
@@ -0,0 +1,95 @@
+
+fun main() {
+    val sehir = "İstanbul"
+    println(sehir)
+    val ulke = "Türkiye"
+    println(ulke)
+    var telefon = "05056531399"
+    println(telefon)
+    val postaKodu = "34000"
+    println(postaKodu)
+    var email = "43elif305@gmail.com"
+    println(email)
+    var meslek = "Yazılım Mühendisi"
+    println(meslek)
+    var stokMiktari = 120
+    println(stokMiktari)
+    var musteriAdi = "Elif Arı"
+    println(musteriAdi)
+    var bakiye = 5000
+    println(bakiye)
+    val dogumGunu = "18.06.2002"
+    println(dogumGunu)
+    var maas = 25000
+    println(maas)
+    var medeniDurum = "Bekar"
+    println(medeniDurum)
+    var urunYorum = "kaliteli"
+    println(urunYorum)
+    val odemeTarihi = "15.04.2025"
+    println(odemeTarihi)
+    var odeme = "Kredi Kartı"
+    println(odeme)
+    var siparisAdeti = 3
+    println(siparisAdeti)
+    var arabaModeli = "Mercedes"
+    println(arabaModeli)
+    val kitapAdi = "Yüzüklerin efendisi Yüzük kardeşliği"
+    println(kitapAdi)
+    val yayinlamaTarihi = "1954"
+    println(yayinlamaTarihi)
+    var indirimMiktari = 20  // yüzde
+    println(indirimMiktari)
+    var odaSayisi = 2
+    println(odaSayisi)
+    val enlem = 41.0082
+    println(enlem)
+    val boylam = 28.9784
+    println(boylam)
+    var urunAdi = "Akıllı Saat"
+    println(urunAdi)
+    var yemekFiyati = 254.99
+    println(yemekFiyati)
+    var marka = "Apple"
+    println(marka)
+    var muzikAdi = "Müslüm Gürses - Sigara"
+    println(muzikAdi)
+    var videoSuresi = 180  // saniye
+    println(videoSuresi)
+    var urunPuani = 4.8
+    println(urunPuani)
+    var resimAdi = "manzara.jpg"
+    println(resimAdi)
+    val dosyaFormati = "JPEG"
+    println(dosyaFormati)
+    var renk = "Mavi"
+    println(renk)
+    val renkKodu = "#0000FF"
+    println(renkKodu)
+    var telefonModeli = "iPhone 14"
+    println(telefonModeli)
+    var ekranBoyutu = 6.1  // inc
+    println(ekranBoyutu)
+    var agirlik = 174  // gram
+    println(agirlik)
+    val ulusalGun = "23 Nisan"
+    println(ulusalGun)
+    val tatilGunu = "1 Ocak"
+    println(tatilGunu)
+    var rezervasyonTarihi = "20.07.2025"
+    println(rezervasyonTarihi)
+    var sokakAdi = "Bağdat Caddesi"
+    println(sokakAdi)
+    var otobusHatti = "34AS"
+    println(otobusHatti)
+    var kalanDakika = 15
+    println(kalanDakika)
+    var takipKodu = "ABC123456XYZ"
+    println(takipKodu)
+    var kuponSuresi = "30 gün"
+    println(kuponSuresi)
+    var kuponKodu = "INDIRIM30"
+    println(kuponKodu)
+    var faturaAdresi = "Kadıköy, İstanbul"
+    println(faturaAdresi)
+}
